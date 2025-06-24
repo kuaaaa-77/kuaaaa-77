499198300b18f2ab6849f310ade67f7c4dbe5349
kuaaaa-77 <nxmbboyz@gmail.com>
2025-06-25 01:36:13 +0700

    Delete project directory

--- project/backend/menu_left.php
+++
@@ -1,8 +0,0 @@
-<div class="list-group">
-	
-	<a href="admin.php" class="list-group-item list-group-item-action">ຈັດການຜູ້ດູແລລະບົບ</a>
-	<a href="member.php" class="list-group-item list-group-item-action">ຈັດການສະມາຊິກ</a>
-	<a href="type.php" class="list-group-item list-group-item-action">ຈັດການປະເພດສິນຄ້າ</a>
-	<a href="product.php" class="list-group-item list-group-item-action">ຈັດການຈັດການສິນຄ້າ</a>
-	<a href="../logout.php" class="list-group-item list-group-item-action" onclick="return confirm('ທ່ານຕ້ອງການອອກຈາກລະບົບຫຼືບໍ່?')">ອອກຈາກລະບົບ</a>
-</div>
